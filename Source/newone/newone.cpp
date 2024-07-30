ba8e52562eab2381736b3fd64dd65c45199b2318
soma-web <andi@pixel-maniacs.com>
2024-07-30 11:46:48 +0200

    Init

---
+++ Source/newone/newone.cpp
@@ -0,0 +1,7 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "newone.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, newone, "newone" );
+ 
